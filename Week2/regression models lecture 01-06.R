a25d9e87c86d4e825c20473546db6c667e2cb6d9
jmacarter <jeffcarter@mac.com>
2016-03-27 09:56:24 -0500

    directory structure

--- Week2/regression models lecture 01-06.R
+++ Week2/regression models lecture 01-06.R
@@ -1,3 +1,7 @@
+library(UsingR)
+data(diamond)
+library(ggplot2)
+
 #Code
 data(diamond)
 y <- diamond$price; x <- diamond$carat; n <- length(y)
@@ -6,14 +10,15 @@ e <- resid(fit)
 yhat <- predict(fit)
 max(abs(e -(y - yhat)))
 max(abs(e - (y - coef(fit)[1] - coef(fit)[2] * x)))
-
+sum(e)
+sum(e * x)
 
 #Residuals are the signed length of the red lines
 plot(diamond$carat, diamond$price,  
      xlab = "Mass (carats)", 
      ylab = "Price (SIN $)", 
      bg = "lightblue", 
-     col = "black", cex = 2, pch = 21,frame = FALSE)
+     col = "black", cex = 1.1, pch = 21,frame = FALSE)
 abline(fit, lwd = 2)
 for (i in 1 : n) 
      lines(c(x[i], x[i]), c(y[i], yhat[i]), col = "red" , lwd = 2)
@@ -39,6 +44,14 @@ g = g + geom_point(size = 7, colour = "black", alpha = 0.4)
 g = g + geom_point(size = 5, colour = "red", alpha = 0.4)
 g
 
+#Residual plot
+g = ggplot(data.frame(x = x, y = resid(lm(y ~x))), aes(x = x, y = y))
+g = g + geom_hline(yintercept = 0, size = 2);
+g = g + geom_point(size = 7, color="black", alpha = 0.4)
+g = g + geom_point(size = 5, color="red", alpha=0.4)
+g = g + xlab("X") + ylab("Residual")
+g
+
 
 #Heteroskedasticity
 x <- runif(100, 0, 6); y <- x + rnorm(100,  mean = 0, sd = .001 * x); 
@@ -59,6 +72,7 @@ g = g + xlab("X") + ylab("Residual")
 g
 
 
+
 #Diamond data residual plot
 diamond$e <- resid(lm(price ~ carat, data = diamond))
 g = ggplot(diamond, aes(x = carat, y = e))
@@ -70,8 +84,23 @@ g = g + geom_point(size = 5, colour = "blue", alpha=0.2)
 g
 
 
+e = c(resid(lm(price ~ 1, data = diamond)),
+      resid(lm(price ~ carat, data = diamond)))
+fit = factor(c(rep("Itc", nrow(diamond)),
+               rep("Itc, slope", nrow(diamond))))
+g = ggplot(data.frame(e = e, fit = fit), aes(y = e, x = fit, fill = fit))
+g = g + geom_dotplot(binaxis = "y", stackdir = "center", binwidth = 20)
+g = g + xlab("Fitting approach")
+g = g + ylab("Residual price")
+g
+
+
 #Diamond example
 y <- diamond$price; x <- diamond$carat; n <- length(y)
 fit <- lm(y ~ x)
 summary(fit)$sigma
 sqrt(sum(resid(fit)^2) / (n - 2))
+
+
+#R-squared
+example("anscombe")
