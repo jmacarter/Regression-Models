c73edfb17a7332292d5ca45a4ab97c7f149c3b58
jmacarter <jmacarter@users.noreply.github.com>
2016-03-30 16:40:27 -0500

    Added files via upload

---
+++ Week3/regression models 02-02.r
@@ -0,0 +1,31 @@
+require(datasets); data(swiss); require(GGally); require(ggplot2)
+g = ggpairs(swiss, lower = list(continuous = "smooth"),params = c(method = "loess"))
+g
+
+summary(lm(Fertility ~ . , data = swiss))
+
+summary(lm(Fertility ~ . , data = swiss))$coefficients
+
+summary(lm(Fertility ~ Agriculture, data = swiss))$coefficients
+
+
+
+#How can adjustment reverse the sign of an effect? Let's try a simulation.
+n <- 100; x2 <- 1 : n; x1 <- .01 * x2 + runif(n, -.1, .1); y = -x1 + x2 + rnorm(n, sd = .01)
+summary(lm(y ~ x1))$coef
+summary(lm(y ~ x1 + x2))$coef
+
+dat = data.frame(y = y, x1 = x1, x2 = x2, ey = resid(lm(y ~ x2)), ex1 = resid(lm(x1 ~ x2)))
+
+g = ggplot(dat, aes(y = y, x = x1, colour = x2))
+g = g + geom_point(colour="grey50", size = 5) + geom_smooth(method = lm, se = FALSE, colour = "black") 
+g = g + geom_point(size = 4) 
+g
+
+g2 = ggplot(dat, aes(y = ey, x = ex1, colour = x2))  
+g2 = g2 + geom_point(colour="grey50", size = 5) + geom_smooth(method = lm, se = FALSE, colour = "black") + geom_point(size = 4) 
+g2
+
+#What if we include an unnecessary variable?
+z <- swiss$Agriculture + swiss$Education
+lm(Fertility ~ . + z, data = swiss)
