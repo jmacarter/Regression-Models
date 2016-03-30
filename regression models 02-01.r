aa5591700f4349c837086c51d3728f4c0d1b6dc5
jmacarter <jmacarter@users.noreply.github.com>
2016-03-30 16:40:39 -0500

    Delete regression models 02-01.r

--- regression models 02-01.r
+++
@@ -1,7 +0,0 @@
-n = 100; x = rnorm(n); x2 = rnorm(n); x3 = rnorm(n)
-y = 1 + x + x2 + x3 + rnorm(n, sd = .1)
-ey = resid(lm(y ~ x2 + x3))
-ex = resid(lm(x ~ x2 + x3))
-sum(ey * ex) / sum(ex ^ 2)
-coef(lm(ey ~ ex - 1))
-coef(lm(y ~ x + x2 + x3)) 
